63b5c936a2f49ec088fc8c1064e3362677c22980
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2022-12-23 10:04:19 -0500

    socketio

---
+++ typescript_tuto/app.js
@@ -0,0 +1,6 @@
+var message = "Hello TypeScript!";
+// create a new heading 1 element
+var heading = document.createElement("h1");
+heading.textContent = message;
+// add the heading to the document body
+document.body.appendChild(heading);
---
+++ typescript_tuto/app.ts
@@ -0,0 +1,8 @@
+let message: string = "Hello TypeScript!";
+// create a new heading 1 element
+
+let heading = document.createElement("h1");
+heading.textContent = message;
+
+// add the heading to the document body
+document.body.appendChild(heading);
