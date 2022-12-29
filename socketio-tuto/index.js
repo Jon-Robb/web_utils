84949efda1613f076a1712d91b95e99698cb17ae
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2022-12-29 06:04:25 -0500

    chat fonctionnel

--- socketio-tuto/index.js
+++ socketio-tuto/index.js
@@ -3,11 +3,23 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+// socket.io initialize a new instance of socket.io by passing the http (the HTTP server) object
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 // we define a route handler / that gets called when we hit our website home
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+
+
 
 // we make the http server listen on port 3000
 server.listen(3000, () => {
