59d680973780916fae27cdfb12f891d10c4d9b49
Jonathan Robinson <robinsonjonathan240817@gmail.com>
2022-12-30 15:21:53 -0500

    added lines

---
+++ typescrypt-react/src/components/FirstComponent.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+let img = "./img/apple.png";
+export default class FirstComponent extends React.Component {
+    render() {
+        return (
+           <div>
+            <h3>
+                A simple react component example with typescript
+            </h3>
+            <div>
+                <img src={img} />
+            </div>
+            <p>this component shows the apple logo</p>
+           </div>
+        );
+    }
+}
